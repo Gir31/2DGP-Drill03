592f7bb4d277651b167f98f807c16cb3974e9400
JHG@LT <rpg2657@gmail.com>
2024-09-24 11:14:06 +0900

    2DGP-Drill03 (2021184002 길준형)

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -9,40 +9,34 @@ boy = load_image('character.png')
 def draw_boy(x, y):
     clear_canvas_now()
     boy.draw_now(x, y)
-    delay(0.05)  
+    delay(0.01)  
 
 def run_top():
-    print('TOP')
     for x in range(0, 800,10):
         draw_boy(x, 550)
     pass
 
 def run_right():
-    print('RIGHT')
     for y in range(550, 0, -10):
         draw_boy(800, y)
     pass
 
 def run_bottom():
-    print('BOTTOM')
     for x in range(800, 0, -10):
-        draw_boy(x, 50)
+        draw_boy(x, 0)
     pass
 
 def run_left():
-    print('LEFT')
     for y in range(0, 550, 10):
         draw_boy(0, y)
     pass
 
 def triangle_left():
-    print('TRIANGLE LEFT')
     for h in range(0, 400, 4):
         draw_boy(h, h)
     pass
 
 def triangle_right():
-    print('TRIANGLE RIGHT')
     cx, cy = 400, 400
     for h in range(0, 400, 4):
         x, y = cx + h, cy - h
@@ -50,7 +44,6 @@ def triangle_right():
     pass
 
 def run_rectangle():
-    print('RECTANGLE')
     run_top()
     run_right()
     run_bottom()
@@ -58,7 +51,6 @@ def run_rectangle():
     pass
 
 def run_circle():
-    print('CIRCLE')
 
     r, cx, cy = 300,800//2, 600//2
 
@@ -71,8 +63,7 @@ def run_circle():
     pass
 
 def run_triangle():
-    print('TRIANGLE')
-    #run_bottom()
+    run_bottom()
     triangle_left()
     triangle_right()
     pass
@@ -80,9 +71,9 @@ def run_triangle():
 
 
 while (True):
-    #run_circle()
-    #run_rectangle()
+    run_circle()
+    run_rectangle()
     run_triangle()
-    break
+    
 
 close_canvas()
