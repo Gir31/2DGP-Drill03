da3e6dfc7182a038f03017ec38778073c25c3d23
JHG@LT <rpg2657@gmail.com>
2024-09-24 09:53:35 +0900

    프린트 통해서 루프가 정상적으로 진행됨

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -2,15 +2,17 @@ from pico2d import *
 import math
 
 def run_rectangle():
+    print('RECTANGLE')
     pass
 
 def run_circle():
+    print('CIRCLE')
     pass
     
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
 
 while (True):
     run_rectangle()
